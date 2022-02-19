4ff4fe3d874d843970cb62f2812188db23223928
HeyaIe <bryngx@gmail.com>
2022-02-19 17:15:46 -0500

    added comments

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,33 +1,39 @@
+// addition operator
 function additionFunction(){
     var result = 3 + 1;
     
     document.getElementById("Math").innerHTML = result;
 }
 
+// subtraction operator
 function subtractionFunction(){
     var result = 5 - 2;
 
     document.getElementById("subtraction").innerHTML = result;
 }
 
+// multiplication operator
 function multiplication(){
     var result = 3 * 3;
 
     document.getElementById("multiplication").innerHTML = result;
 }
 
+// division operator
 function division(){
     var result = 100 / 10;
 
     document.getElementById("division").innerHTML = result;
 }
 
+// multiple operators
 function multipleOperators(){
     var result = (50 + 60) / 2 - 10 * 3;
 
     document.getElementById("multipleOp").innerHTML = result;
 }
 
+// modulus operator
 function modulusOperator(){
     var result = 32 % 5;
 
@@ -41,7 +47,7 @@ function negationOperator(){
     document.getElementById("negative").innerHTML = -result;
 }
 
-//
+// using Math.PI
 function degToRad(degrees){
     result = degrees * (Math.PI / 180);
 
