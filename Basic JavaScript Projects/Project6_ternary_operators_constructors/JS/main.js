e4088c5f60ebc3fcbd5a82f03657015dcac72936
HeyaIe <bryngx@gmail.com>
2022-02-21 15:13:51 -0500

    Added functions to project6_ternary_operators_constructors folder

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -14,3 +14,32 @@ function voteFunction(){
     canVote = (age >= 18) ? "You are old enough":"You are too young";
     document.getElementById("Vote").innerHTML = canVote + " to vote.";
 }
+
+// Constructor
+function myFunc(name, age, canVote){
+    this.name = name;
+    this.age = age;
+    canVote = (age >= 18) ? "old enough":"too young";
+    this.canVote = canVote;
+}
+// Initializing new object
+var Bryan = new myFunc("Bryan", 27);
+
+// Remodelized vote function
+function voteFunc2(){
+    document.getElementById("New_and_This").innerHTML = 
+        Bryan.name + " is " + Bryan.age + " years old and is "
+        + Bryan.canVote + " to vote.";
+}
+
+// Nested function
+function pickNumber(num){
+    this.number = num;
+    addFive();
+
+    // add 5 to number function
+    function addFive(){
+        return number += 5;
+    }
+    document.getElementById("Nested_Function").innerHTML = number;
+}
