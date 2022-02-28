a3c779ea6178cd23792c6a7ea58714400204a667
HeyaIe <bryngx@gmail.com>
2022-02-28 13:34:07 -0500

    Added comments

--- Basic JavaScript Projects/PizzaProject/Pizza_JavaScript/Pizza.js
+++ Basic JavaScript Projects/PizzaProject/Pizza_JavaScript/Pizza.js
@@ -1,14 +1,17 @@
 function getReceipt(){
+    // Initalize string
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
     for(var i=0; i<sizeArray.length; ++i){
         if(sizeArray[i].checked){
+            // slectedSize = checked pizza size
             var selectedSize = sizeArray[i].value;
             text1 = text1 + selectedSize + "<br>";
         }
     }
+    // Update sizeTotal
     if(selectedSize === "Personal Pizza"){
         sizeTotal = 10;
     }
@@ -24,6 +27,7 @@ function getReceipt(){
     else if(selectedSize === "Extra Large Pizza"){
         sizeTotal = 20;
     }
+    // Update runningTotal
     runningTotal = sizeTotal;
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("size text1: " + text1);
@@ -32,20 +36,29 @@ function getReceipt(){
     // These variables will get passed on to each function
     getTopping(runningTotal, text1);
 }
-
+// This function checks for selected toppings, gives 1 free topping,
+// displays total and user's order
 function getTopping(runningTotal, text1){
+    // Initalize counter
     var toppingTotal = 0;
+    // Initalize empty array
     var selectedTopping = [];
+
     var toppingArray = document.getElementsByClassName("toppings");
     for(var j=0; j<toppingArray.length; ++j){
+        // Checks to see if any element in the array is checked
         if(toppingArray[j].checked){
+            // Adds selected topping to array
             selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item: (" + toppingArray[j].value + ")");
+            // Updates text1 string
             text1 = text1 + toppingArray[j].value + "<br>";
         }
     }
+    // Assign total number of selected toppings to toppingCount
     var toppingCount = selectedTopping.length;
     if(toppingCount > 1){
+        // Ensures that 1 topping is less than total
         toppingTotal = (toppingCount - 1);
     }
     else{
